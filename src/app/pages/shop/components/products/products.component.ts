3468155889e9c8777d3381c8678a30822f922e69
3djDavid <dj.vivanco@gmail.com>
2022-05-31 21:34:10 -0400

    service post

--- src/app/pages/shop/components/products/products.component.ts
+++ src/app/pages/shop/components/products/products.component.ts
@@ -3,7 +3,8 @@ import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { DialogProductComponent } from '../dialog-product/dialog-product.component';
 
 import { Product } from '../../store/list';
-
+import { PerfilService } from '@app/services/perfil.service';
+import { AuthService } from '@app/services/auth.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -11,25 +12,73 @@ import { Product } from '../../store/list';
 })
 
 export class ProductsComponent implements OnInit {
-
   //recibo la lista de productos desde el padre
+  productId!: number;
+  cantidad!: number;
+  @Input() products !: Product[];
+  pEmail: any;
 
-  @Input() products ! : Product[]; 
+  constructor(
+    private dialog: MatDialog,
+    private perfilService: PerfilService,
+    public authService: AuthService
+  ) {
+    this.pEmail = ''
 
-  constructor(private dialog: MatDialog) {    
   }
 
-  ngOnInit(): void {
-    console.log("los productos: ", this.products);
+  ngOnInit(): void { 
+
+  
   }
 
-  openDialog(){
+  openDialog(id: number) {
+   
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = false;
     dialogConfig.autoFocus = true;
-
     this.dialog.open(DialogProductComponent, dialogConfig);
+
+    this.productId = id
+    this.createCarrito();
   }
+
+  createCarrito() { 
+
+    this.getEmailservice();
+
+    this.perfilService.createCarrito(this.pEmail,this.productId, 1)
+    .subscribe({
+      next: (res: any) => {
+        console.log("el res es:,", res)
+        
+      },
+      error: (e: any) => {
+        console.log("el error es:", e)
+      },
+      complete: () => {
+        console.info('completed')
+      }
+    }) 
+  }
+
+  getEmailservice(){
+    this.authService.email$
+    .subscribe({
+      next: (res: any) => {
+        console.log("el res es:,", res)
+        this.pEmail = res.email
+      },
+      error: (e: any) => {
+        console.log("el error es:", e)
+      },
+      complete: () => {
+        console.info('completed')
+      }
+    }) 
+  }
+
+
 
 
 
--- src/app/services/perfil.service.ts
+++ src/app/services/perfil.service.ts
@@ -1,5 +1,4 @@
 
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment'
@@ -10,8 +9,6 @@ import { environment } from '../../environments/environment'
 export class PerfilService {
 
   data: any;
- 
-
   private URL = environment.baseUrl
  
   constructor(
@@ -25,10 +22,17 @@ export class PerfilService {
     return this.http.get<any>(this.URL + `/api/perfil/${email}`)
   }
 
-
-
   putMisDatos(email:any, dataBody:any) {
    
     return this.http.put<any>(this.URL + `/api/perfil/${email}`, dataBody)
   }
+  
+  
+  createCarrito(email:any, productId:number, cant: number){
+    const ordenPedido= 'O-002'
+    console.log("createCarrito en post", email, productId, cant, ordenPedido)
+    return this.http.post<any>(this.URL + `/api/carrito`, {email, productId, cant, ordenPedido})
+
+  }
+
 }
